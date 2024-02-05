7356f2b575d8c693c46412a22f8f170da889955d
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2024-02-05 15:06:35 -0300

    up

--- account/main.tf
+++ account/main.tf
@@ -6,13 +6,3 @@ module "vpc" {
   source = "../modules/vpc"
   vpc_name       = var.account
 }
-
-module "ec2" {
-    source = "./modules/ec2"
-
-    ami                = data.aws_ami.latest_amazon_linux.id
-    instance_type      = var.ec2_instance_type
-    key_name           = var.ec2_key_name
-    security_group_ids = [module.vpc.default_security_group_id]
-    instance_name      = var.ec2_instance_name
-}
